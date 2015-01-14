2baec0b54a55962c60530b71a4856bfc1cbe50a6
Tony DiPerna <dipernaa@msoe.edu>
2015-01-14 11:31:30 -0600

    took out spring training clock

--- public/scripts/controllers/MainCtrl.js
+++ public/scripts/controllers/MainCtrl.js
@@ -1,15 +1,3 @@
-angular.module('myApp').controller('MainCtrl', function($scope, $timeout) {
-    $scope.isWinter = new Date().getTime() < new Date(2015, 1, 18).getTime();
+angular.module('myApp').controller('MainCtrl', function($scope) {
 
-    if($scope.isWinter) {
-        tick();
-    }
-    function tick() {
-        var millis = new Date(2015, 1, 18) - new Date();
-        $scope.days = (millis / (1000*3600*24)).toFixed(0);
-        $scope.hours = (millis / (1000*3600)).toFixed(0) - ($scope.days * 24);
-        $scope.minutes = (millis / (1000 * 60)).toFixed(0) - ($scope.days * 24 * 60) - ($scope.hours * 60)
-        $scope.seconds = (millis / (1000)).toFixed(0) - ($scope.days * 24 * 3600) - ($scope.hours * 3600) - ($scope.minutes * 60) + 30;
-        $timeout(tick, 1000);
-    }
 })
