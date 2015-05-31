39cd8638a6dddb0b68ef1ad42919a386ffb37fe8
unknown <dipernaa@msoe.edu>
2015-05-30 23:03:37 -0500

    changed css and updated resume

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
 var express = require('express');
-//var request = require('request'); //Used for external http requests
-//var mysql = require('mysql');
-//var qr = require('qr-image');
 var path = require('path');
 var cors = require('cors');
-//var fs = require('fs'); //Used to read local files
 var app = express();
 
 app.use(bodyParser.json());
@@ -18,7 +14,6 @@ app.set('views', __dirname + '/public/views');
 app.engine('html', require('ejs').renderFile);
 
 app.use(express.static(path.join(__dirname, 'public')));
-//var filePath = path.join(__dirname, 'public/text_files/MegaCustomOSHASigns.csv'); //Filepath for fs to read from
 
 /**
  * Displays the main page
